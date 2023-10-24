9e5814c0074d0c3370adbe6309b9e4a7830f3d53
hiphopkorzhik <148270384+hiphopkorzhik@users.noreply.github.com>
2023-10-24 21:21:04 +0300

    app v.1.0 (#1)
    * writing code v1.0
    
    * writing tests v1.0
    
    ---------
    
    Co-authored-by: popov artem <pupanalupe@gmail.com>

---
+++ cmake-build-debug/lib/googletest-1.14.0/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/PopovArtem/CLionProjects/ProjectPart1/lib/googletest-1.14.0/googlemock
+# Build directory: C:/Users/PopovArtem/CLionProjects/ProjectPart1/cmake-build-debug/lib/googletest-1.14.0/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
